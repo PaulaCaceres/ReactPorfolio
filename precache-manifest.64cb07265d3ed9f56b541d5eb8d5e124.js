fbb5c28032dbb0760a446175979c9d72ce324d0f
PaulaCaceres <paulacaceres.dis@gmail.com>
2020-07-14 20:45:17 -0300

    Updates

---
+++ precache-manifest.64cb07265d3ed9f56b541d5eb8d5e124.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3184ee00ed567ac705642a48bbfadb",
+    "url": "/ReactPorfolio/index.html"
+  },
+  {
+    "revision": "18070368f0c72989cbea",
+    "url": "/ReactPorfolio/static/css/main.ec7a4f02.chunk.css"
+  },
+  {
+    "revision": "eb91d72f85d3f03f8d42",
+    "url": "/ReactPorfolio/static/js/2.303ee9fd.chunk.js"
+  },
+  {
+    "revision": "ac00860f10b301ce4ff76aaa802ce348",
+    "url": "/ReactPorfolio/static/js/2.303ee9fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18070368f0c72989cbea",
+    "url": "/ReactPorfolio/static/js/main.5a06d2ea.chunk.js"
+  },
+  {
+    "revision": "7aa96248176f5ec26b95",
+    "url": "/ReactPorfolio/static/js/runtime-main.e263af8d.js"
+  },
+  {
+    "revision": "b89267290c0df2e03ae1b60bd14109c8",
+    "url": "/ReactPorfolio/static/media/DMSans-Italic.b8926729.ttf"
+  },
+  {
+    "revision": "3e7f038b85daa739336e4a3476c687f2",
+    "url": "/ReactPorfolio/static/media/DMSans-Regular.3e7f038b.ttf"
+  },
+  {
+    "revision": "d5274d4c900c515a5f28644594266e4d",
+    "url": "/ReactPorfolio/static/media/Logo-iniciales-blanco.d5274d4c.svg"
+  },
+  {
+    "revision": "d38d66acea55dfc2b12a507279269869",
+    "url": "/ReactPorfolio/static/media/Logo-nombre-blanco.d38d66ac.svg"
+  },
+  {
+    "revision": "c5f946497cde714355b07feed3391a1c",
+    "url": "/ReactPorfolio/static/media/mypic.c5f94649.jpg"
+  }
+]);
